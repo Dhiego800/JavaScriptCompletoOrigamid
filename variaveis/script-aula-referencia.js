63e4fcdd084ac6287cbd47f408af3566c0032a1b
Diego Miranda <diego.miranda@enext.local>
2019-10-14 15:53:23 -0300

    [JavaScript Para iniciantes] Aula Variaveis 0201

---
+++ variaveis/script-aula-referencia.js
@@ -0,0 +1,41 @@
+// Declarando variaveis Primeiro passso
+
+var nome = 'Diego';
+var idade = 22;
+var possoiFaculdade = true;
+console.log(nome, idade, possoiFaculdade);
+
+// Alterando valor final utilizando variaveis 
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+
+// Criando variavel sem declarar a palavra VAR varias vezes, apenas utilizando virgula
+var sobreNome = 'Rafael',
+  cidade = 'Rio';
+console.log(sobreNome, cidade);
+
+// variavel sem valor
+var semDefinir;
+
+// variavel sempre é "jogada para cima do codigo"
+var comida;
+console.log(comida);
+comida = "Pizza";
+console.log(comida);
+
+var time = 'timeDoGrau';
+console.log(time);
+var time = 'timeOsCorte';
+console.log(time);
+
+// exercicio variaveis sem valor
+
+const moto;
+const categoria;
+var peso;
+var cilindrada;
+var preso;
---
+++ variaveis/script.js
@@ -0,0 +1,14 @@
+// Exercicio 1
+
+var nome = 'Diego';
+var idade = '22';
+var comidaFavorita = 'frango';
+var valorComida = '12';
+
+var time, 
+  professor,
+  pais,
+  endereco,
+  rua; 
+
+console.log(nome, idade, comidaFavorita)
