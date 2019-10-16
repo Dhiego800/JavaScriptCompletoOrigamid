db6926799d8919c3ec526d5d915934c7457b51c7
Diego Miranda <diego.miranda@enext.local>
2019-10-16 16:08:23 -0300

    [JavaScript Para iniciantes]Aula Números e Operadores 1 203

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,16 @@
+var total = 10 + 9 + 10;
+  console.log(total);
+
+var divisao = 100 / 5;
+  console.log(divisao);
+
+var modulo = 343242424322 % 3;
+  console.log(modulo);
+
+//Não será retornado numero, e sim uma string, então NaN é true
+var testeNaN = 'test 100' / 2;
+console.log(isNaN(testeNaN));
+
+//retorna um numero então NaN é false
+var testeNaN2 = '100' / 2;
+console.log(isNaN(testeNaN2));
