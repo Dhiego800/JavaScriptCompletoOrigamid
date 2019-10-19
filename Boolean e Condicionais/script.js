f63c3493d203a4154db8f77b6bc15979b5620bf6
Diego Miranda <diego.miranda@enext.local>
2019-10-19 19:36:42 -0300

    [JavaScript Para iniciantes]Aula Boolean e Condicionais 1 204

---
+++ Boolean e Condicionais/script.js
@@ -0,0 +1,61 @@
+var possuiGraduacao = true;
+var possuiDotourado = true;
+
+if(possuiGraduacao) {
+  console.log('Possui Graduação (É verdadeiro)');
+  
+} else if(possuiDotourado) {
+  console.log('Possui Dotourado!!!');
+
+} else {
+  console.log('Não possui NADA');
+}
+
+
+
+/* 
+IF que retorna FALSE
+
+  if(false)
+  if(0)
+  if(NaN)
+  if(null)
+  if(undefined)
+  if('') 
+
+IF que retorna TRUE
+
+  if(true)
+  if(1)
+  if('  ')
+  if(-5)
+  if({})
+
+IF operador de negação
+
+  if(!true) retorna FALSE
+  if(!1) retorna FALSE
+  if(!'') retorna TRUE
+  if(!undefined) retorna TRUE
+  if(!!' ') retorna TRUE
+  if(!!'') retorna FALSE
+
+
+  Operadores de comparação
+
+  10 > 5; TRUE
+  5 > 10; FALSE
+  20 < 10; FALSE
+  10 <= 10; TRUE
+  10 >= 11; FALSE
+
+  Operador de comparação
+
+  10 == '10'; TRUE
+  10 == 10; TRUE
+  10 === '10'; FALSE
+  10 === 10; TRUE
+  10 != 15; TRUE
+  10 != '10'; FALSE 
+  10 !== '10'; TRUE
+*/
