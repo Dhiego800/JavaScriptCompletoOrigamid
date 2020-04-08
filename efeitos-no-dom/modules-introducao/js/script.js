a3b7f94607251adcc81596da7f0477781bce7b10
unknown <diiego.mm@hotmail.com>
2020-04-08 04:54:23 -0300

    Aula 0502 Modules 1 [Concluída]

---
+++ efeitos-no-dom/modules-introducao/js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+
+
+
+
+
+
+
+
+/*OBSERVAÇÕES
+    Named Exports
+    você pode exportar mais de um valor. Quando for importar
+utilize as chaves para especificar cada valor. O nome importado de
+
+import{arq.import, arq.import } devem contém mesmo nome do export*/
