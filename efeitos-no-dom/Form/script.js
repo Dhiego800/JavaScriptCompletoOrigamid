9c2c0b5f50f939fc7375f1307efe6ed9895a8feb
Dhiego800 <diiego.mm@hotmail.com>
2020-04-24 08:30:27 -0300

    init aula 0512

---
+++ efeitos-no-dom/Form/script.js
@@ -0,0 +1,29 @@
+// Quais erros o codigo a baixo vai retornar ?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+}
+// console.log(cor, marca, portas);
+
+const dois = 2;
+function somarDois(x) {
+  return x + dois;
+}
+
+function dividirDois(x) {
+  return x / dois;
+}
+
+console.log(somarDois(4))
+console.log(dividirDois(6));
+
+//O que fazer para o total retornar 500
+const numero = 50;
+
+for(let i = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
