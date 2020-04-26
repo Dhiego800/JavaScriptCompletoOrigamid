51656ff2af5a14a4b3c3999c5838691f4ccc0b47
Dhiego800 <diiego.mm@hotmail.com>
2020-04-26 20:52:53 -0300

    Aula 0512 Forms 2 [Concluída]

--- efeitos-no-dom/Form/script.js
+++ efeitos-no-dom/Form/script.js
@@ -1,7 +1,27 @@
-const contato = document.forms[0];
-const texto = document.querySelector('.texto');
-
+/* 
 function handleKeyUp(event){
+  const target = event.target;
+
+  if(!target.checkValidity()){
+    target.classList.add('invalido');
+    contato.nome.setCustomValidity('Campo NOME deve ser preenchido');
+    target.nextElementSibling.innerText = target.validationMessage;
+
+  }  
   texto.innerText = event.target.value;
 };
-contato.addEventListener('change', handleKeyUp);
+
+
+function handleKeyUp(event){
+  document.body.style.backgroundColor = event.target.value;  
+};
+ */
+const contato = document.forms[0];
+const texto = document.querySelector('.texto');
+const dado = {};
+
+function handleChange(event){
+  dado[event.target.name] = event.target.value;
+};
+
+contato.addEventListener('change', handleChange);
