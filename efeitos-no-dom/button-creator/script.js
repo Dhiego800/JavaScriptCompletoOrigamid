feb97d1f9680aa7dd8e9362a23aaa748f17f0176
unknown <diiego.mm@hotmail.com>
2020-04-27 14:12:41 -0300

    0515 Aula LocalStorage [Concluída]

--- efeitos-no-dom/button-creator/script.js
+++ efeitos-no-dom/button-creator/script.js
@@ -36,11 +36,27 @@ const handleStyle = {
 
 function handleChange(event){
   const name = event.target.name;
-  const value = event.target.value;  
+  const value = event.target.value; 
+
   handleStyle[name](value);
+  saveValues(name, value);
   showCss();
 };
 
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValue(){
+  const properties = Object.keys(localStorage);
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
+  });
+  showCss();
+};
+setValue();
+
 function showCss() {
   cssText.innerHTML = '<span>' + btn.style.cssText.split('; ').join(';</span><span>');
-}
+};
