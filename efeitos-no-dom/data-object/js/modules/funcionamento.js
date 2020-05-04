d942d4e38bb55e745119b325a72a0f523702d83b
unknown <diiego.mm@hotmail.com>
2020-05-04 15:01:44 -0300

    Atualizando repositorio[Data-Object], aula 0607 History API [Concluída]

---
+++ efeitos-no-dom/data-object/js/modules/funcionamento.js
@@ -0,0 +1,33 @@
+{/*new Date()
+  O construtor Date cria um objeto contento valores como 
+  mês, dia, ano e MediaSource. A data é baseada no rélogio interno
+  do computador.
+
+  const agora = new Date() Semana Mês Ano HH:MM:SS GTM
+
+  agora.getDate() Dia
+  agora.getDay() Dia da semana ex: 5 = fri
+  agora.getMonth() Número dia mês
+  agora.getFullYear() Ano
+  agora.getHours() Hora
+  agora.getMinutes() Minutos
+  agora.getTime() ms desde 1970
+  agora.getUTCHours() -3 Brasília 
+
+  const agora = new Date();
+  const natal = new Date('Dec 24 2020 23:59');
+  
+  function convetDias(time){
+    return time / (24 * 60 * 60 * 1000);
+  };
+  
+  const diasAgora = convetDias(agora);
+  const diasPromocao = convetDias(natal);
+  const faltamNatal = diasAgora - diasPromocao;
+*/}
+
+
+
+export default function initFuncionamento(){
+
+};
--- javascript-assincrono/history-api/script.js
+++ javascript-assincrono/history-api/script.js
@@ -11,10 +11,44 @@
 /*
 	Só é possivel usar o evento popstate, se o pushState()
 	for definido manualmente.
+	
+	async function fetchPage(url) {
+		const pageReponse = await fetch(url);
+		const pageText = await pageReponse.text();
+		window.history.pushState(null, null, url);
+	}
 */
 
-async function fetchPage(url) {
-	const pageReponse = await fetch(url);
-	const pageText = await pageReponse.text();
-	window.history.pushState(null, null, url);
+const links = document.querySelectorAll('a');
+
+function handleClick(event) {
+	event.preventDefault();
+	fetchPage(event.target.href);
+	window.history.pushState(null, null, event.target.href);
+};
+
+async function fetchPage(url){
+	document.querySelector('.content').innerHTML = 'Carregando . . .'
+	const pageResponse = await fetch(url);
+	const pageText = await pageResponse.text();
+	replaceContent(pageText);
 }
+
+function replaceContent(newText) {
+	const newHtml = document.createElement('div');
+	newHtml.innerHTML = newText;
+	
+	const oldContent = document.querySelector('.content');
+	const newContent = newHtml.querySelector('.content');
+
+	oldContent.innerHTML = newContent.innerHTML;
+	document.title = newHtml.querySelector('title').innerText;
+}
+
+window.addEventListener('popstate', () => {
+	fetchPage(window.location.href);
+})
+
+links.forEach(link => {
+	link.addEventListener('click', handleClick);
+});
